c15b89159815245b403d9e97b854c2e56e5b68b5
drsenthilkumarp <senthilkumarp@bitsathy.ac.in>
2024-07-17 22:11:15 +0530

    Update Install package.bat

--- Install package.bat
+++ Install package.bat
@@ -1,1 +1,11 @@
-
+choco install zoom 
+choco install googlechrome 
+choco install vlc
+choco install vscode 
+choco install mingw
+choco install arduino 
+choco install nodepadplusplus 
+choco install pdfview 
+choco install teamviewer 
+choco install anydesk 
+choco install blender
